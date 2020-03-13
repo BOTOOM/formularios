bf1ed24324db94b5216df2aa79be57961d7df689
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-03-13 16:03:33 -0500

    validaciones template apellido y correo

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -9,7 +9,9 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
 
   usuario = {
-    nombre: ''
+    nombre: 'edwar',
+    apellido: 'diaz',
+    correo: 'eddiazruiz@acm.org'
   };
 
   constructor() { }
