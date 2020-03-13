cc7db776708f7df4e045d68c8f879f1a6607a1e9
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-03-13 16:44:27 -0500

    servicio de paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +20,7 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from '../../servides/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -14,9 +15,13 @@ export class TemplateComponent implements OnInit {
     correo: 'eddiazruiz@acm.org'
   };
 
-  constructor() { }
+  constructor( private piasService: PaisService ) { }
 
   ngOnInit(): void {
+    this.piasService.getPaises()
+    .subscribe( paises => {
+      console.log(paises);
+    } );
   }
 
 
